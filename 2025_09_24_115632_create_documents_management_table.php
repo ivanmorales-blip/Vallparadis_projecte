0d7025b0ab6dcdf393bc2edbee9d4056822bbbd6
ivanmorales-blip <ivan.morales@inslapineda.cat>
2025-09-29 09:47:15 +0200

    Update 2025_09_24_115632_create_documents_management_table.php

--- 2025_09_24_115632_create_documents_management_table.php
+++ 2025_09_24_115632_create_documents_management_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('documents_management', function (Blueprint $table) {
             $table->id();
+            $table->foreign('centre_id')->references('id')->on('centre')->onDelete('cascade');
+            $table->string('tipus', 255);
+            $table->datetime('data');
+            $table->text('descripcio');
+            $table->foreign('professional_id')->references('id')->on('professional')->onDelete('cascade');
+            $table->text('arxiu');
             $table->timestamps();
         });
     }
