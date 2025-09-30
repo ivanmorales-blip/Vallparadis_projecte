fc21c98c2a414a44df5a593cf8d67fc217a575f1
eduardmas10 <eduardmaslledo98@gmail.com>
2025-09-30 12:48:13 +0200

    cambios

--- app/Models/Profesional.php
+++ app/Models/Profesional.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profesional extends Model
 {
-    //
+    public function uniform(): HasMany{
+        return $this->hasMany(Uniformity::class);
+    }
 }
