553dfda09b2cc3baa8f5a5b8e8bc3a824bdc7a63
eduardmas10 <eduardmaslledo98@gmail.com>
2025-10-06 09:32:48 +0200

    cambios

---
+++ resources/views/center/formulario_alta.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    aqui va la vista
+    <form>
+<!-- Nom -->
+<div>
+<label for="nom">Nom *</label>
+<input id="nom" name="nom" type="text" required>
+</div>
+
+
+<!-- Adreça -->
+<div>
+<label for="adresa">Adreça</label>
+<textarea id="adresa" name="adresa" rows="2"></textarea>
+</div>
+
+
+<!-- Telefon -->
+<div>
+<label for="telefon">Telèfon *</label>
+<input id="telefon" name="telefon" type="tel" required>
+</div>
+
+
+<!-- Mail -->
+<div>
+<label for="mail">Correu electrònic *</label>
+<input id="mail" name="mail" type="email" required>
+</div>
+
+
+<div>
+<button type="submit">Enviar</button>
+<button type="reset">Netejar</button>
+</div>
+</form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CenterController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/altaCenter',[CenterController::class,"create"]);
