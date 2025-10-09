df5e43d4e71391172175c90affe02c66fa3d12fc
ivanmorales-blip <ivan.morales@inslapineda.cat>
2025-10-09 10:08:59 +0200

    test

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -17,7 +17,7 @@
     <h1 class="text-4xl font-bold text-blue-600 mb-4">Hello, World!</h1>
     <p class="text-gray-700">If you see this styled text, Tailwind is working 🎉</p>
     <button class="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
-      Click Me
+      Click Me 2
     </button>
   </div>
 </body>
