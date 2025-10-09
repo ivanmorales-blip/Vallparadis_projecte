b44739ecbab08145a47a44db73a1526ee127d163
ivanmorales-blip <ivan.morales@inslapineda.cat>
2025-10-09 10:05:59 +0200

    Initial commit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
     <h1><a href="{{route('centers.index')}}">Listar centros</a></h1>
     <h1><a href="{{route('centers.create')}}"> Alta centro</a></h1>
     <h1><a href="{{route('profesional.index')}}">Listar Profesionals</a></h1>
     <h1><a href="{{route('profesional.create')}}"> Alta Profesionals</a></h1>
     <h1><a href="{{route('projectes_comissions.index')}}">Listar Projectes i comissions</a></h1>
     <h1><a href="{{route('projectes_comissions.create')}}"> Alta  Projectes i comissions</a></h1>
+    <div class="text-center p-6 bg-white rounded-lg shadow-lg">
+    <h1 class="text-4xl font-bold text-blue-600 mb-4">Hello, World!</h1>
+    <p class="text-gray-700">If you see this styled text, Tailwind is working 🎉</p>
+    <button class="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+      Click Me
+    </button>
+  </div>
 </body>
 </html>
