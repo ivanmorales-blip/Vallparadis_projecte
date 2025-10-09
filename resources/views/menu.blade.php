53bd68fdbdc8330f4e22cd2a177015fbf6a23615
izanmongerueda <izan.monge@inslapineda.cat>
2025-10-09 12:24:40 +0200

    commit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -17,7 +17,7 @@
     <h1><a href="{{route('projectes_comissions.create')}}"> Alta  Projectes i comissions</a></h1>
     <div class="text-center p-6 bg-white rounded-lg shadow-lg">
     <h1 class="text-4xl font-bold text-blue-600 mb-4">Hello, World!</h1>
-    <p class="text-gray-700">If you see this styled text, Tailwind is working 🎉</p>
+    <p class="text-gray-700">If you see this styled text, Tailwind is not working 🎉</p>
     <button class="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
       Click Me 2
     </button>
