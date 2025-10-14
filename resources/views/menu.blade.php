820f1a11340c481ceb8c17d74ea8a1efa40a90ba
ivanmorales-blip <ivan.morales@inslapineda.cat>
2025-10-14 10:45:12 +0200

    tailwnd-progress

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -14,20 +14,29 @@
     <img src="{{ asset('images/logo_1.png') }}" alt="Logo Vallparadis" class="h-[90px] w-auto"/>
   </div>
 
+    <div class="flex-1"></div>
+
   <!-- Search bar -->
-  <div class="flex-grow max-w-xl mx-4">
+  <div class="ml-auto w-48 pr-4">
     <input 
       type="text" 
-      placeholder="Search..." 
+      placeholder="Cercar..." 
       class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
   </div>
 
+  <div class="flex items-center space-x-6 pr-4">
+    <a href="{{ url('/menu') }}" class="bg-[#F97800] hover:bg-[#cc6e00] text-white px-4 py-2 rounded transition cursor-pointer">
+        Retornar al menu >
+    </a>
+  </div>
+
   <!-- Logout button -->
   <button 
-    class="bg-[#F97800] text-white px-4 py-2 rounded hover:bg-red-600 transition"
+    class="bg-[#F97800] text-white px-4 py-2 rounded hover:bg-[#cc6e00] transition"
+
   >
-    Logout
+    Tancar sessió
   </button>
     </header>
 
