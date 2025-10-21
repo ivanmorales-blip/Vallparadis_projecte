27f8d513122a9617183fc6e037793115bde23f9a
ivanmorales-blip <ivan.morales@inslapineda.cat>
2025-10-21 11:40:48 +0200

    sprint2

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,18 +4,18 @@
 
 <div class="container mx-auto px-4 py-8 grid gap-8 md:grid-cols-3">
 
-  <!-- Professionals Card -->
-  <div class="bg-white shadow-lg border border-gray-300 rounded-lg p-6 flex flex-col">
-    <h2 class="text-2xl text-gray-700 font-bold mb-4 border-b border-gray-300 pb-2">Professionals</h2>
-    <a href="{{ route('profesional.index') }}" class="text-[#F97800] hover:underline mb-2">Listar Professionals</a>
-    <a href="{{ route('profesional.create') }}" class="text-[#F97800] hover:underline">Alta Professionals</a>
-  </div>
-
   <!-- Centres Card -->
   <div class="bg-white shadow-lg border border-gray-300 rounded-lg p-6 flex flex-col">
     <h2 class="text-2xl text-gray-700 font-bold mb-4 border-b border-gray-300 pb-2">Centres</h2>
     <a href="{{ route('centers.index') }}" class="text-[#F97800] hover:underline mb-2">Listar Centros</a>
     <a href="{{ route('centers.create') }}" class="text-[#F97800] hover:underline">Alta Centro</a>
+  </div>
+
+  <!-- Professionals Card -->
+  <div class="bg-white shadow-lg border border-gray-300 rounded-lg p-6 flex flex-col">
+    <h2 class="text-2xl text-gray-700 font-bold mb-4 border-b border-gray-300 pb-2">Professionals</h2>
+    <a href="{{ route('profesional.index') }}" class="text-[#F97800] hover:underline mb-2">Listar Professionals</a>
+    <a href="{{ route('profesional.create') }}" class="text-[#F97800] hover:underline">Alta Professionals</a>
   </div>
 
   <!-- Projectes Card -->
